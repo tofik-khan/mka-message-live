be2e47a3f87753507281810636aab5aa0b41e132
Tofik Khan <tofikahmad@live.com>
2022-06-09 13:44:48 -0700

    Removing switch/case from access denied

--- components/access-denied.tsx
+++ components/access-denied.tsx
@@ -1,7 +1,6 @@
 import { signIn } from "next-auth/react"
 
-export default function AccessDenied({ response }) {
-  console.log(response)
+export default function AccessDenied() {
   return (
     <>
       <h1>Access Denied</h1>
@@ -18,48 +17,4 @@ export default function AccessDenied({ response }) {
       </p>
     </>
   )
-  switch (response) {
-    case "nosignin":
-      return (
-        <>
-          <h1>Access Denied - Not Signed in</h1>
-          <p>
-            <a
-              href="/api/auth/signin"
-              onClick={(e) => {
-                e.preventDefault()
-                signIn()
-              }}
-            >
-              You must be signed in to view this page
-            </a>
-          </p>
-        </>
-      )
-      break
-    case "unauthorized":
-      return (
-        <>
-          <h1>Access Denied - Invalid User</h1>
-          <p>
-            You do not have access to this page. Contact the developer to gain
-            access.
-          </p>
-          <p>
-            <a
-              href="/api/auth/signin"
-              onClick={(e) => {
-                e.preventDefault()
-                signIn()
-              }}
-            >
-              Back to Signin
-            </a>
-          </p>
-        </>
-      )
-      break
-    default:
-      break
-  }
 }
